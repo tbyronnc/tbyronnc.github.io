03f96f038ba2d467352438284e381d77859327fd
Terry Byron <tbyron@ncsu.edu>
2022-01-21 13:58:04 -0500

    Updated

---
+++ ocr.php
@@ -0,0 +1,56 @@
+<?php
+require_once 'layout.inc';
+start_page();
+?>
+
+<p>
+<div> 
+<strong>OBSTACLE COURSE RACES</strong>
+<div style="float: right;"><a href="https://www.mudrunguide.com/directory/usa/north-carolina-nc/"><img src="MRGLogo2015.png" height="70"></a></div>
+&nbsp;<br>
+<div style=float: clear: right">&nbsp;</div>
+</div>
+<b>2022 </a></b>
+<ul>
+<li>NC - <a href="https://ruggedmaniac.com/">Rugged Maniac</a> - 3 Mile</li>
+</ul>
+<b>2021 </b>
+<ul>
+<li>VA - <a href="https://bonefrogchallenge.com/">Bonefrog</a> Tier 1 - 9 Mile</li>
+<li>NC - <a href="https://bonefrogchallenge.com/">Bonefrog</a> Challenge+Sprint - 6+3 Mile </li>
+</ul>
+
+<b>2019</b>
+<ul>
+<li>NC - <a href="https://bonefrogchallenge.com/">Bonefrog</a> Challenge - 6 Mile</li>
+<li>NC - <a href="https://savagerace.com/">SavageRace</a> - 6 Mile</li>
+<li>MD - <a href="https://savagerace.com/">SavageRace</a> - 6 Mile</li>
+</ul>
+&nbsp;<br>
+
+<b>2018</b>
+<ul>
+<li>NC - <a href="https://www.spartan.com/">Spartan</a> Sprint - 3 Mile</li>
+<li>NC - <a href="https://www.spartan.com/">Spartan</a> Super - 6 Mile</li>
+<li>NC - <a href="https://www.spartan.com/">Spartan</a> Beast - 13 Mile</li>
+</ul>
+
+<b>2017</b>
+<ul>
+<li>NC - <a href="http://www.dragonocr.com/">Dragon Obstacle Course Race</a> - 3 Mile</li>
+</ul>
+
+<b>2016</b>
+<ul>
+<li>NC - The Ninja Challenge - 3 Mile</li>
+<li>NC - Warrior Wolf Race - 3 Mile</li>
+</ul>
+
+<b>2015</b>
+<ul>
+<li>NC - The Ninja Challenge - 3 Mile</li>
+</ul>
+
+<?php
+end_page();
+?>
